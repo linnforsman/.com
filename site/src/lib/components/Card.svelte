0482ec1851969534fefbb4c134f0cd6d9cddd2fc
linnforsman <linnforsman@protonmail.com>
2023-02-15 17:11:31 +0100

    style: Make Card component tall

--- site/src/lib/components/Card.svelte
+++ site/src/lib/components/Card.svelte
@@ -3,9 +3,19 @@
 <style>
   .card {
     display: flex;
+    justify-content: first baseline;
+    width: 10rem;
+    height: 20rem;
     flex: 1 1 auto;
     flex-direction: column;
     padding: 1rem;
     border-radius: 3px;
+  }
+
+  @media screen and (max-width: 425px) {
+    .card {
+      width: auto;
+      height: auto;
+    }
   }
 </style>
