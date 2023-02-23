c1ba0621ce3da4fd47629080edbedc2f867df988
linnforsman <linnforsman@protonmail.com>
2023-02-23 20:36:05 +0100

    feat: Make Card component dynamic
    - using props
    - {#if}, {else} blocks
    - add selectors for styling

--- site/src/lib/components/Card.svelte
+++ site/src/lib/components/Card.svelte
@@ -1,15 +1,71 @@
-<div class="card"><slot /></div>
+<script>
+  // Declare props to Card
+  import SanityImage from "$lib/components/SanityImage.svelte";
+  export let CARDTYPE;
+  export let CARDTITLE;
+  export let DESCRIPTION;
+  export let DATE;
+  export let IMAGE;
+  export let TYPE;
+</script>
 
-<style>
+<div class="card">
+  <div class="cardbar">
+    <small class="metadata">CARD_TYPE={CARDTYPE}</small>
+    {#if DATE}
+      <small class="metadata">
+        {new Date(DATE).toLocaleDateString("en", {
+          month: "long",
+          year: "numeric",
+        })}
+      </small>
+    {/if}
+  </div>
+  <div>
+    {#if TYPE}
+      <span class="type">
+        {TYPE}
+      </span>
+    {/if}
+    <h2>{CARDTITLE}</h2>
+    {#if DESCRIPTION}
+      <p>{DESCRIPTION}</p>
+    {/if}
+    {#if IMAGE}
+      <SanityImage image={IMAGE} maxWidth={550} />
+    {/if}
+  </div>
+</div>
+
+<style lang="scss">
+  h2,
+  span {
+    text-transform: uppercase;
+  }
   .card {
-    display: flex;
-    justify-content: first baseline;
-    width: 10rem;
-    height: 20rem;
+    display: grid;
+    justify-content: center;
+    width: 15rem;
+    height: 30rem;
     flex: 1 1 auto;
     flex-direction: column;
-    padding: 1rem;
-    border-radius: 3px;
+    border-radius: 5px;
+    grid-template-columns: 15% 85%;
+    padding-right: 15px;
+    .cardbar {
+      text-align: start;
+      transform: rotate(180deg);
+      height: 30rem;
+    }
+    .metadata {
+      writing-mode: vertical-lr;
+      font-size: 10px;
+      text-transform: uppercase;
+      padding: 10px;
+    }
+    .type {
+      padding-left: 0px;
+    }
   }
 
   @media screen and (max-width: 425px) {
