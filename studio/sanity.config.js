2b361f2fc74448102673a9fc6c6332beb8b9d8b4
linnforsman <linnforsman@protonmail.com>
2023-02-24 14:02:22 +0100

    feat: Add Code Input as a plugin

--- studio/sanity.config.js
+++ studio/sanity.config.js
@@ -2,13 +2,14 @@ import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
+import {codeInput} from '@sanity/code-input'
 
 export default defineConfig({
   name: 'default',
   title: 'Linn Forsman',
   projectId: '6akigqdh',
   dataset: 'production',
-  plugins: [deskTool(), visionTool()],
+  plugins: [deskTool(), visionTool(), [codeInput()]],
 
   schema: {
     types: schemaTypes,
