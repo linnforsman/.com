d33d7fedde3ed8865d7f30c4d4f7ef97c76247f1
Linn Forsman <linnforsman@protonmail.com>
2024-01-08 21:57:39 +0000

    feat: Import script for web analytics

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -2,6 +2,11 @@
   import "$lib/styles/global.scss";
   import CustomCursor from "$lib/components/CustomCursor.svelte";
   import Footer from "$lib/components/Footer.svelte";
+
+  // Imports for view of user insights to Vercel's Web Analytics dashboard.
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
+  inject({ mode: dev ? "development" : "production" });
 </script>
 
 <CustomCursor />
