5219e34e50c7094c851224fe3dce877297ba381e
linnforsman <linnforsman@protonmail.com>
2024-01-17 17:10:44 +0100

    feat: Update Introduction section

--- site/src/lib/components/Introduction.svelte
+++ site/src/lib/components/Introduction.svelte
@@ -1,52 +1,49 @@
 <script>
   import { fade, fly } from "svelte/transition";
   import CallToAction from "$lib/components/CallToAction.svelte";
-  import LF from "$lib/assets/Title.svg";
-  export let sitename;
-  export let subtitle;
+  export let herotitle, title, roles;
 </script>
 
-<!-- Introduction -->
+<!-- identity -->
 
-<section in:fade={{ x: 50, duration: 1000 }} out:fade class="introduction">
-  <div in:fly={{ x: 50, duration: 1250 }} class="introduction-content">
-    <h1 class="title gradient-light-text">{sitename}</h1>
-    <h2 class="subtitle">{subtitle}</h2>
-    <CallToAction />
+<section in:fade={{ x: 50, duration: 1000 }} out:fade>
+  <div class="identity-item" in:fly={{ x: 50, duration: 1250 }}>
+    <h2 class="title-2">{title}</h2>
+  </div>
+  <div>
+    <h1 class="title">{herotitle}</h1>
+  </div>
+  <div class="identity-item">
+    <h3 class="title-3">{roles}</h3>
   </div>
 </section>
+<CallToAction />
 
 <!-- End of Introduction.svelte -->
 
 <style lang="scss">
-  .introduction {
-    position: relative;
+  section {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
     width: 100vw;
     height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    .introduction-content {
-      position: relative;
-      padding: 0;
-      margin: 1rem;
-    }
-    .title {
-      font-size: min(16vw, 10rem);
-    }
-    .subtitle {
-      text-transform: uppercase;
-    }
   }
-  @media (max-width: 786px) {
-    .introduction {
-      justify-content: center;
-      .title {
-        font-size: min(16vw, 3.5rem);
-        .subtitle {
-          font-size: min(16vw, 1rem);
-        }
-      }
-    }
+  .identity-item {
+    display: flex;
+    align-items: center;
+  }
+  .title {
+    font-size: min(10vw, 25rem);
+    text-transform: uppercase;
+  }
+
+  .title-2 {
+    font-size: min(10vw, 2.5rem);
+    text-transform: uppercase;
+  }
+  .title-3 {
+    font-size: min(16vw, 1.5rem);
+    text-transform: uppercase;
   }
 </style>
