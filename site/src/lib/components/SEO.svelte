eb187868b11f35cdb0ce58a043fac23f67bb18bd
linnforsman <linnforsman@protonmail.com>
2024-01-19 12:00:09 +0100

    feat: Add SEO component into page

---
+++ site/src/lib/components/SEO.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let title, desc, keywords, slug, img;
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta name="description" content={desc} />
+  <meta name="keywords" content={keywords} />
+  <meta name="slug" content={slug} />
+  <meta property="og:image" content={img} />
+</svelte:head>
--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+  import SEO from "$lib/components/SEO.svelte";
   import Introduction from "$lib/components/Introduction.svelte";
   import Skillset from "$lib/components/Skillset.svelte";
   import AOI from "$lib/components/AOI.svelte";
@@ -6,6 +7,13 @@
 </script>
 
 {#each data.content as data}
+  <SEO
+    title={data.seoTitle}
+    desc={data.seoDescription}
+    keywords={data.seoKeywords}
+    slug={data.seoSlug}
+    img={data.seoImage}
+  />
   <Introduction herotitle={data.name} title={data.intro} roles={data.roles} />
 
   <Skillset skillset={data.skillset} />
